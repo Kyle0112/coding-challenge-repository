2ee0ee285b9b979903270fd967bc2e812bf3a4a4
Kyle Domingo <kyledomingo.hs@gmail.com>
2024-03-07 21:00:51 +0800

    Included explanations on different parts of the code

--- problem_set_1/palindrome_pairs.go
+++ problem_set_1/palindrome_pairs.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// isPalindrome checks if a string is palindrome
 func isPalindrome(s string) bool {
 	for i := 0; i <= len(s)/2; i++ {
 		if s[i] != s[len(s)-i-1] {
@@ -13,11 +14,13 @@ func isPalindrome(s string) bool {
 	return true
 }
 
+// finds all pairs of indices
 func palindromePairs(words []string) [][]int {
 	var result [][]int
-
+	//iterate over all pairs of words
 	for i := 0; i < len(words); i++ {
 		for j := 0; j < len(words); j++ {
+			//avoid comparing words to itself
 			if i != j {
 				concatenated := words[i] + words[j]
 				if isPalindrome(concatenated) {
@@ -33,7 +36,7 @@ func palindromePairs(words []string) [][]int {
 
 func main() {
 
-	inputWords := []string{"bat", "tab", "cat"}
+	inputWords := []string{"bat", "tab", "cat", "rat", "tar"}
 	result := palindromePairs(inputWords)
 	fmt.Println(result)
 }
